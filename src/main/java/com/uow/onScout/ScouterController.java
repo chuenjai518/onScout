77b357bc61cf8e03d7ca31d37fbb02feb5169e9b
felix3456789 <thc950303@gmail.com>
2019-03-08 02:12:13 +0800

    scouterNav + scoutProcess Review

--- src/main/java/com/uow/onScout/ScouterController.java
+++ src/main/java/com/uow/onScout/ScouterController.java
@@ -1,8 +1,22 @@
 package com.uow.onScout;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.uow.Model.User;
+import com.uow.Model.UserInfo;
 
 @Controller
 public class ScouterController {
-
+	@GetMapping("scouter")
+	public String index(Model model, HttpSession session) {
+		// model.addAttribute("user", (User)session.getAttribute("user"));
+		
+		return "Scouter/scoutProcess";
+	}
 }
