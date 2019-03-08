9a81b57e1f40c43d2282185e57210c5a89e5ee1f
kaman0916 <47221374+kaman0916@users.noreply.github.com>
2019-03-08 12:36:21 +0800

    hi

--- src/main/java/com/uow/onScout/ScoutController.java
+++ src/main/java/com/uow/onScout/ScoutController.java
@@ -1,6 +1,10 @@
 package com.uow.onScout;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ScoutController {
