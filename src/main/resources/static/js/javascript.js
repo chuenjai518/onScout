b37027141e544d1b7b1cde4c609ac90d33396c93
Felix <thc950303@gmail.com>
2019-03-13 18:25:56 +0800

    edit form disable to enable

--- src/main/resources/static/js/javascript.js
+++ src/main/resources/static/js/javascript.js
@@ -4,8 +4,11 @@ $(document).ready(function(){
 function enable(){
 	if($('input').prop('disabled') == true){
 		$('input').prop('disabled',false);
+		$('#submit').prop('disabled',false);
 	}else{
 		$('input').prop('disabled',true);
+		$('#submit').prop('disabled',true);
 	}
+	
 	
 }
