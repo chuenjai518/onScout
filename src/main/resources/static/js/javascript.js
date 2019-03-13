4cb3ed5adcdc8c805a4e58df0c7eae4b28d95269
kaman0916 <47221374+kaman0916@users.noreply.github.com>
2019-03-13 22:45:27 +0800

    Update javascript.js

---
+++ src/main/resources/static/js/javascript.js
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+=======
+$(document).ready(function(){
+	
+});
+function enable(){
+	if($('input').prop('disabled') == true){
+		$('input').prop('disabled',false);
+		$('#submit').prop('disabled',false);
+	}else{
+		$('input').prop('disabled',true);
+		$('#submit').prop('disabled',true);
+	}
+	
+	
+}
+>>>>>>> 09f5e91fe8fab36bb2f907398611550dc28729b9
