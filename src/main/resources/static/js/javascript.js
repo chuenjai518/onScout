06a4fb71e695cd6e15da7f6f698e74e30622e167
Felix <thc950303@gmail.com>
2019-03-13 18:16:49 +0800

    scouterProfile js

--- src/main/resources/static/js/javascript.js
+++ src/main/resources/static/js/javascript.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
 	
-	
 });
+function enable(){
+	if($('input').prop('disabled') == true){
+		$('input').prop('disabled',false);
+	}else{
+		$('input').prop('disabled',true);
+	}
+	
+}
