c3ab372cbbd96ac660001b90f3fc79521b3e7e6f
Wilson Chu <wilsonchu11@gmail.com>
2019-03-14 01:22:28 +0800

    ?

--- src/main/java/com/uow/DAO/ScoutDAO.java
+++ src/main/java/com/uow/DAO/ScoutDAO.java
@@ -23,7 +23,7 @@ public class ScoutDAO {
 	
 	public UserInfo getScout(String username) {
 		
-		String sql = "Select p.username, p.firstName, p.lastName, p.HKID, p.DOB, p.gender, p.address, p.phoneNum, p.email FROM PersonalInfo p, User u WHERE u.roleID = 1 AND u.userName = p.userName AND p.username = ?;";
+		String sql = "Select u.username, roleID, firstName, lastName, HKID, DOB, gender, address, phoneNum, email, region, district, scoutGroup, DOI from User u left join PersonalInfo p on u.username = p.username Where u.username = ? and disable = 0 and roleID = 1";
 		RowMapper<UserInfo> rowMapper = new UserInfoRowMapper();
 		try{
 			return this.db.queryForObject(sql, rowMapper, username);
