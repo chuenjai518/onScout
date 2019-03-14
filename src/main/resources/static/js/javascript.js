bf782cfa3fd693ce6c1b67c334758fe6f0a38db7
felix3456789 <thc950303@gmail.com>
2019-03-14 22:05:11 +0800

    scouter view pdf

--- src/main/resources/static/js/javascript.js
+++ src/main/resources/static/js/javascript.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+	$("img").click(function() {
+       var id = $(this).attr('id');
+       console.log(id);
+     $("iframe").attr("src", "showPDF/"+id);
+            
+    });
+	
 });
 function enable(){
 	if($('input').prop('disabled') == true){
