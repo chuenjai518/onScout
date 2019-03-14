53ec11281b918f293c5ff6b8fcecdc5a2472aa9d
felix3456789 <thc950303@gmail.com>
2019-03-15 01:55:36 +0800

    ajax

--- src/main/resources/static/js/javascript.js
+++ src/main/resources/static/js/javascript.js
@@ -1,46 +1,27 @@
 $(document).ready(function(){
-	$("img").click(function() {
+	$("sAw").click(function() {
        var id = $(this).attr('id');
        console.log(id);
-     $("iframe").attr("src", "showPDF/"+id);
-            
+     $("iframe").attr("src", "showPDF/"+id);     
     });
-	
-	$('#adward').click(function(){
+	$('#Award').click(function(){
 		 $.ajax({
-             url: "Handler.ashx",   //存取Json的網址             
-             type: "POST",
+			 type: "POST",
+			 url: "http://localhost:8081/onScout/api/getSPAFinishDate/scout",   // 存取Json的網址
              cache:false,
              dataType: 'json',
-             data:{id:"111", user:"frank"},
-             //contentType: "application/json",
+             // contentType: "application/json",
              success: function (data) {
-
-                 //方法一 (回傳多筆資料)                
-                 for (var i = 0; i < data.length; i++) {
-                     alert("name=" + data[i]["欄位名稱"]);    
-                 }
-
-                 //方法二 (回傳多筆資料)
-                 var i = 0;                    
-                 $.each(data, function () {
-                     alert(data[i].欄位名稱);    
-                     i++;
-                 });
-
-                 //方法三 (回傳單筆資料)
-                 $.each(data, function (index, element) {
-                     alert(element);                      
-                 });
+            	 var str="<ul>";    
+            	 $.each(data,function(i,n){   
+            		console.log(n);
+            		//change #testSpan to the correct field
+            		//just copy and paste and n[{the column in ajax}]
+            		$("#testSpan").append(n['username']);    
+            	 });
              },
-
-             error: function (xhr, ajaxOptions, thrownError) {
-                 alert(xhr.status);
-                 alert(thrownError);
-             }
          });
 	});
-	
 });
 function enable(){
 	if($('input').prop('disabled') == true){
