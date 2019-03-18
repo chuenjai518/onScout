10d9d79ecbda87fde8cc6a285543e0938f75a2d5
Wilson Chu <wilsonchu11@gmail.com>
2019-03-18 15:56:15 +0800

    ?

--- src/main/java/com/uow/DAO/ScouterDAO.java
+++ src/main/java/com/uow/DAO/ScouterDAO.java
@@ -189,7 +189,7 @@ public class ScouterDAO {
 	}
 	
 	public void checkSubTask(String username, int questID) {
-		Date date;
+		String date;
 		String missionNumSQL = "Select missionNum From SubTask where questID = ?";
 		int missionNum = db.queryForObject(missionNumSQL, Integer.class, questID);
 		int missionEnd = questID + 9;
@@ -197,11 +197,11 @@ public class ScouterDAO {
 		int count = db.queryForObject(countSQL, Integer.class, questID, missionEnd, username);
 		if(count >= missionNum) {
 			String latestDateSQL = "Select FinishDate From CompletedQuest where questID >= ? and questID < ? and username = ? ORDER BY FinishDate DESC limit 1";
-			date = db.queryForObject(latestDateSQL, Date.class, questID, missionEnd, username);
+			date = db.queryForObject(latestDateSQL, String.class, questID, missionEnd, username);
 			String updateDateSQL = "Insert Into CompletedQuest(questID, FinishDate, username) " + "VALUES(?,?,?)";
 			db.update(updateDateSQL,questID, date,username);
 			questID = questID - (questID % 100);
-			checkAward(username, questID);
+			checkTask(username, questID);
 		}
 	}
 	
