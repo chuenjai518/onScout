534c4df2f462824d1cdd1582f21754bfcf713741
chuenjai518 <37447380+chuenjai518@users.noreply.github.com>
2019-03-18 19:02:53 +0800

    asdasfasfgasfgasgasgae

--- src/main/java/com/uow/onScout/ScouterController.java
+++ src/main/java/com/uow/onScout/ScouterController.java
@@ -87,8 +87,7 @@ public class ScouterController {
 		UserInfo user = scouterService.getScoutInfo(username);
 		model.addAttribute("openSSA", scouterService.checkSSA(username));
 		model.addAttribute("openSAA", scouterService.checkSAA(username));
-//		model.addAttribute("openCSA", scouterService.checkCSA(username));
-		model.addAttribute("openCSA", true);
+		model.addAttribute("openCSA", scouterService.checkCSA(username));
 		model.addAttribute("user", user);
 		return "Scouter/scoutProcessDetail";
 	}
